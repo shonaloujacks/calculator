98fe276e86ef09e33bcb89fcd5371ca1ff240cbf
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-07-31 14:26:19 +0100

    updating directory structure

---
+++ calculator.js
@@ -0,0 +1,36 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function  multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+function operate (firstNumber, operator, secondNumber) {
+
+ if (operator === "+") {
+    return add(firstNumber, secondNumber); 
+ }
+ else if (operator === "-") {
+    return subtract(firstNumber, secondNumner);
+ }
+else if (operator === "/") {
+    return divide(firstNumber, secondNumber); 
+}
+else if (operator === "*") {
+    return multiply(firstNumber, secondNumber); 
+}
+} 
